0f495a68a9b2f26a809332f9cd89ac8d539817b8
FRANCIS DARKO <fd92uk@gmail.com>
2026-01-13 09:21:53 +0000

    Initial commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,12 +66,15 @@ SIMPLE_JWT = {
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtppro.zoho.eu'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'francis@dataclubcenter.com'
-EMAIL_HOST_PASSWORD = 'uksg41201312212A.'
-DEFAULT_FROM_EMAIL = 'francis@dataclubcenter.com'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
 
 
 # Application definition
